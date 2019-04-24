37e5b8042e4ddb2e93c40e5d760b967f6aa6a6a4
JPBlancoDB <jpblancodb@gmail.com>
2019-04-24 19:55:28 +0200

    Rename tasks-project to web

--- apps/tasks-project/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: "tasks-project",
-  preset: "../../jest.config.js",
-  coverageDirectory: "../../coverage/apps/tasks-project/",
-  snapshotSerializers: [
-    "jest-preset-angular/AngularSnapshotSerializer.js",
-    "jest-preset-angular/HTMLCommentSerializer.js"
-  ]
-};
